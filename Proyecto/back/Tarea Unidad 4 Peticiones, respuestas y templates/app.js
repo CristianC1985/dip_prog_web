6c74771580c1f77b7438f0898895a0bfa7f4cfe5
Cristian Coronel <cristian.coronel85@gmail.com>
2023-07-05 01:06:17 -0300

    change

--- Proyecto/back/Tarea Unidad 4 Peticiones, respuestas y templates/app.js
+++ Proyecto/back/Tarea Unidad 4 Peticiones, respuestas y templates/app.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({
-  secret: '',
+  secret: 'qwertyasdzxc123',
   reserve: false,
   saveUninitialized: true
 
