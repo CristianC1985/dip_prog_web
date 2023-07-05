adfde15c020a6dab63ee59ff44c36badcf9c916a
Cristian Coronel <cristian.coronel85@gmail.com>
2023-07-05 00:09:33 -0300

    app

--- Proyecto/back/app.js
+++ Proyecto/back/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/admin/login')
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/admin/login', loginRouter);
 
 app.get('/prueba', function(req, res){
   res.send('Hola soy la pagina de prueba');
