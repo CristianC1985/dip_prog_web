a6f3f9c4079089cd78965c57b137469f467457ac
Cristian Coronel <cristian.coronel85@gmail.com>
2023-08-30 02:18:52 -0300

    llll

--- Proyecto/back/app.js
+++ Proyecto/back/app.js
@@ -1,64 +1,79 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-require('dotenv').config();
-var pool = require('./models/db');
+require("dotenv").config();
+var pool = require("./models/db");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var loginRouter = require('./routes/admin/login');
-var adminRouter = require('./routes/admin/novedades');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var loginRouter = require("./routes/admin/login");
+var adminRouter = require("./routes/admin/novedades");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/admin/login', loginRouter);
-app.use('/admin/novedades', adminRouter);
+
+
+secured = async (req, res, next) => {
+  try {
+    console.log(req.session.id_usuario);
+    if (req.session.id_usario) {
+      next();
+    } else {
+      res.redirect("/admin/login");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/admin/login", loginRouter);
+app.use("/admin/novedades", adminRouter);
 //consultas
-var pool = require('./models/db')
-pool.query("select * from empleados").then(function(resultados) {
-  console.log(resultados)
+var pool = require("./models/db");
+pool.query("select * from empleados").then(function (resultados) {
+  console.log(resultados);
 });
 
-app.get('/prueba', function(req, res){
-  res.send('Hola soy la pagina de prueba');
-})
-app.get('/destacados', function (req, res) {
-    res.send('Hola soy la pagina destacados');
-  })
+app.get("/prueba", function (req, res) {
+  res.send("Hola soy la pagina de prueba");
+});
+app.get("/destacados", function (req, res) {
+  res.send("Hola soy la pagina destacados");
+});
 
-app.get('/ingreso', function (req, res) {
-    res.send('Hola soy la pagina ingreso');
-  })
+app.get("/ingreso", function (req, res) {
+  res.send("Hola soy la pagina ingreso");
+});
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
